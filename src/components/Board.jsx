14a2e5cdbbc841dd802a57d38d84a8d2ad7b7eed
arefin6 <arefinhossain3@gmail.com>
2025-11-08 14:57:55 +0600

    game is working fine

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,6 +4,41 @@ import Square from "./Square";
 const Board = () => {
   const [Squares, setSquares] = useState(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState(true);
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = calculateWinner(Squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (isXNext ? "X" : "O");
+  }
+  if (winner) {
+    alert("Winner: " + winner);
+  }
+
   function handleClick(i) {
     if (Squares[i]) {
       return;
@@ -19,6 +54,7 @@ const Board = () => {
   }
   return (
     <>
+      <div className="mb-4 text-xl">{status}</div>
       <div className="flex">
         <Square value={Squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={Squares[1]} onSquareClick={() => handleClick(1)} />
